7944d9cfd68eb6a5959bf16c5058ae59a2cf59b7
DonghyeonKang <donghyeon3372@gmail.com>
2021-09-26 19:25:31 +0900

    Solve 10814

--- baekjoon/10814.py
+++ baekjoon/10814.py
@@ -2,5 +2,9 @@ num = int(input())
 data = []
 for i in range(num):
     age, name = map(str, input().split(" "))
-    data.append((int(age), name))
-print(data)
+    data.append([int(age), i, name])
+data = sorted(data)
+
+for i in range(num):
+    print(data[i][0], end=" ")
+    print(data[i][2])
