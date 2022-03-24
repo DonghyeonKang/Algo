a3f1e472c619c24f7badd08443ebe7676ae1ad5a
DonghyeonKang <donghyeon3372@gmail.com>
2022-03-24 21:58:01 +0900

    solve 1920 1929

--- python/1920.py
+++ python/1920.py
@@ -1,54 +1,20 @@
-# 입력 받은 리스트를 정렬 후 탐색, 정렬되지 않은 상태로 탐색
+n = int(input())
+v1 = sorted(list(map(int, input().split())))
+m = int(input())
+v2 = list(map(int, input().split()))
 
-# def binarySearch(datalist, num):
-#     while(len(datalist) > 0):   # datalist 의 길이가 없어지면 종료
-#         idx = len(datalist) // 2
-#         if(datalist[idx] == num):   # 값을 찾으면 1
-#             return 1
-#         elif(datalist[idx] > num):  # 찾는 값이 idx가 가리키는 값보다 작을 경우
-#             datalist = datalist[:idx]
-#         else:  # 찾는 값이 idx가 가리키는 값보다 클 경우
-#             datalist = datalist[idx + 1:]
-#     return 0   # 값이 없으면 0
+for i in v2:
+    left = 0
+    right = n - 1
 
-datanumcnt = int(input())
-datalist = list(map(int, input().split(" ")))
-
-positivetmplist = [0]
-positivetmplistlength = 0
-negativetmplist = [0]
-negativetmplistlength = 0
-
-for i in datalist:
-    if i < 0:
-        if negativetmplistlength < i:
-            for j in range(i - negativetmplistlength):
-                negativetmplist.append(0)
-                negativetmplistlength += 1
-            negativetmplist[-negativetmplistlength] = 1
+    while left <= right:
+        mid = (left + right) // 2
+        if v1[mid] == i:
+            print(1)
+            break
+        elif v1[mid] > i:
+            right = mid - 1
         else:
-            print(-i)
-            negativetmplist[-i] = 1        
-    else:
-        if positivetmplistlength < i:
-            for j in range(i - positivetmplistlength):
-                positivetmplist.append(0)
-                positivetmplistlength += 1
-            positivetmplist[positivetmplistlength] = 1
-        else:
-            positivetmplist[i] = 1
-
-findnumcnt = int(input())
-numfind = list(map(int, input().split(" ")))
-
-for i in numfind:
-    if i >= 0:
-        if i <= positivetmplistlength:
-            print(positivetmplist[i])
-        else:
-            print(0)
-    if  i < 0:
-        if -i <= negativetmplistlength:
-            print(negativetmplist[-i])
-        else:
-            print(0)
+            left = mid + 1
+    if left > right:
+        print(0)
