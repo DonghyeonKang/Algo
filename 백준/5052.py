719b6433dc176be15dca989c380b86c065eb33b8
DonghyeonKang <86303312+DonghyeonKang@users.noreply.github.com>
2024-03-14 00:24:30 +0900

    5052

--- 백준/5052.py
+++ 백준/5052.py
@@ -12,72 +12,21 @@ for i in range(test):
     for j in range(n[i]):
         nums[i].append(sys.stdin.readline().rstrip())
 
-# 결과
-result = []
-for i in nums:
-    flag = True
-    for j in range(len(i)):
-        remain = i.copy()
-        del remain[j]
-
-        for k in remain:
-            if len(k) >= len(i[j]) and i[j] == k[:len(i[j])]:
-                flag = False
-    
-    if flag:
-        result.append('YES')
-    else:
-        result.append('NO')
+# 정렬
+for i in range(test):
+    nums[i].sort()
 
 # 결과
-result = []
-for i in nums:
+for i in range(test):
+    tmp = nums[i][0]
     flag = True
-    for j in range(len(i)):
-        a = i[j]
-        for k in i[j + 1:]:
-            if str(a) == str(k)[:len(str(a))]:
-                flag = False
-    
+
+    for j in range(1, len(nums[i])):
+        if len(nums[i][j]) >= len(tmp) and tmp == nums[i][j][:len(tmp)]:
+            print('NO')
+            flag = False
+            break
+        tmp = nums[i][j]
+
     if flag:
-        result.append('YES')
-    else:
-        result.append('NO')
-
-print("\n".join(result))
-
-
-# # 전화번호 목록
-
-# test = int(input())
-# n = []
-# nums = []
-
-# # 입력
-# for i in range(test):
-#     n.append(int(input()))
-#     nums.append([])
-#     for j in range(n[i]):
-#         nums[i].append(int(input()))
-
-# # 정렬
-# for i in nums:
-#     i.sort()
-
-# # 결과
-# result = []
-# for i in nums:
-#     flag = True
-#     for j in range(len(i)):
-#         a = i[j]
-#         for k in i[j + 1:]:
-#             if str(a) == str(k)[:len(str(a))]:
-#                 flag = False
-    
-#     if flag:
-#         result.append('YES')
-#     else:
-#         result.append('NO')
-
-# for i in result:
-#     print(i)
+        print('YES')
